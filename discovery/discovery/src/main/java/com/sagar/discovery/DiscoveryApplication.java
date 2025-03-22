fb2ca4d2ebc3991a3e30f2e5efb2a30f22588b3f
Sagar Barate <sagarbarate2907@com.com>
2025-03-22 08:29:06 +0000

    discovery server config

--- discovery/discovery/src/main/java/com/sagar/discovery/DiscoveryApplication.java
+++ discovery/discovery/src/main/java/com/sagar/discovery/DiscoveryApplication.java
@@ -1,14 +1,13 @@
-package com.example.demo;
+package com.sagar.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class DemoApplication {
+public class DiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DiscoveryApplication.class, args);
 	}
 
 }
